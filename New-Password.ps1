21d76430aa7eb7570a1baf7b687ac838f1ac6ef1
Nicholas Dille <nicholas.dille@mailbox.org>
2014-12-30 13:45:40 +0100

    Custom password generator

---
+++ New-Password.ps1
@@ -0,0 +1,20 @@
+﻿Function New-Password() {
+    Param(
+        [int]$Length=10
+        ,
+        [string[]]$Alphabet
+    )
+
+    if (-Not $Alphabet) {
+        $Alphabet = $null
+        40..126 + 33 + 35..38 | foreach {
+            $Alphabet += ,[char][byte]$_
+        }
+    }
+
+    For ($i = 1; $i –le $Length; $i++) {
+        $TempPassword += ($Alphabet | Get-Random)
+    }
+
+    return $TempPassword
+}
