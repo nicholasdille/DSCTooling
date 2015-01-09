39bf84f7b216c9bca7b3e54df76f794f9ec42b55
Nicholas Dille <nicholas.dille@mailbox.org>
2015-01-09 11:53:59 +0100

    Created function from separate script

--- Functions.ps1
+++ Functions.ps1
@@ -1,4 +1,72 @@
-﻿function ConvertTo-EncryptedString {
+﻿function Set-VmConfiguration {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]
+        $VmHost
+        ,
+        [Parameter(Mandatory=$true)]
+        [string]
+        $VmName
+        ,
+        [Parameter(Mandatory=$true)]
+        [string]
+        $NodeGuid
+        ,
+        [Parameter(Mandatory=$false)]
+        [string]
+        $DomainCredName = 'administrator@demo.dille.name'
+        ,
+        [Parameter(Mandatory=$false)]
+        [string]
+        $LocalCredName = 'administrator@WIN-xxxxxxxx'
+        ,
+        [Parameter(Mandatory=$false)]
+        [string]
+        $RootCaName = 'demo-CA'
+        ,
+        [Parameter(Mandatory=$false)]
+        [string]
+        $LocalBasePath = 'c:\dsc'
+        ,
+        [Parameter(Mandatory=$false)]
+        [string]
+        $IPv4Pattern = '^\d+\.\d+\.\d+\.\d+$'
+    )
+    
+    $DomainCredFile = Join-Path -Path $PSScriptRoot -ChildPath ('Cred\' + $DomainCredName + '.clixml')
+    $CertFile       = Join-Path -Path $PSScriptRoot -ChildPath ('Cert\' + $VmName + '.pfx')
+    $MetaFile       = Join-Path -Path $PSScriptRoot -ChildPath ('Output\' + $NodeGuid + '.meta.mof')
+
+    $LocalCredFile  = Join-Path -Path $PSScriptRoot -ChildPath 'Cred\' + $LocalCredName + '.clixml'
+    $CertCredFile   = Join-Path -Path $PSScriptRoot -ChildPath 'Cred\Certificates.clixml'
+    $CaFile         = Join-Path -Path $PSScriptRoot -ChildPath 'Cert\' + $RootCaName + '.cer'
+
+    Enable-VMIntegrationService -ComputerName $VmHost -VMName $VmName -Name 'Guest Service Interface'
+
+    $Files = $($CertFile, $MetaFile, $CaFile)
+    $Files = foreach ($File in $Files) {
+        $File -imatch '^(\w)\:\\' | Out-Null
+        $File.Replace($Matches[0], '\\' + $env:COMPUTERNAME + '.' + $env:USERDNSDOMAIN + '\' + $Matches[1] + '$\')
+    }
+    Invoke-Command -ComputerName $VmHost -Authentication Credssp -Credential (Import-Clixml -Path $DomainCredFile) -ScriptBlock {
+        foreach ($File in $Using:Files) {
+            Copy-VMFile $Using:VmName -SourcePath $File -DestinationPath $Using:LocalBasePath -CreateFullPath -FileSource Host -Force
+        }
+    }
+
+    $Vm = Get-VM -ComputerName $VmHost -Name $VmName
+    $VmIp = $Vm.NetworkAdapters[0].IPAddresses | where { $_ -match $IPv4Pattern }
+    $CertPass = (Import-Clixml -Path $CertCredFile)
+    Invoke-Command -ComputerName $VmIp -Credential (Import-Clixml -Path $LocalCredFile) -ScriptBlock {
+        Get-ChildItem $Using:LocalBasePath\*.cer | foreach { Import-Certificate -FilePath $_.FullName -CertStoreLocation Cert:\LocalMachine\Root | Out-Null }
+        Get-ChildItem $Using:LocalBasePath\*.pfx | foreach { Import-PfxCertificate -FilePath $_.FullName -CertStoreLocation Cert:\LocalMachine\My -Password $Using:CertPass | Out-Null }
+        Get-ChildItem $Using:LocalBasePath\*.meta.mof | where { $_.BaseName -notmatch 'localhost.meta.mof' } | select -First 1 | Rename-Item -NewName localhost.meta.mof -ErrorAction SilentlyContinue
+
+        Set-DscLocalConfigurationManager -Path $Using:LocalBasePath -ComputerName localhost
+    }
+}
+
+function ConvertTo-EncryptedString {
     param(
         [Parameter(Mandatory=$true,ValueFromPipeline=$true)]
         [ValidateNotNullOrEmpty()]
