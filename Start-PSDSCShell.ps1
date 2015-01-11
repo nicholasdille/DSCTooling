8239c78b628fac5d083ebc462105d34644e139ed
Nicholas Dille <nicholas.dille@mailbox.org>
2015-01-11 14:05:08 +0100

    All functions are now automatically sourced

--- Start-PSDSCShell.ps1
+++ Start-PSDSCShell.ps1
@@ -4,4 +4,5 @@
     Set-Location -Path $PSDSC_BasePath
     . .\Variables.ps1
     . $PSDSC_FunctionsFile
+    Get-ChildItem $PSDSC_BasePath\Functions_*.ps1 | foreach { . .\$($_.Name) }
 } -args $PSScriptRoot
