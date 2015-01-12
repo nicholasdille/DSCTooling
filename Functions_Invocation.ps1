2e865f226106847e20f7cd8745ec467f8eb214dc
Nicholas Dille <nicholas.dille@mailbox.org>
2015-01-12 10:27:58 +0100

    Moved DSC resource downloader to library

--- Functions_Invocation.ps1
+++ Functions_Invocation.ps1
@@ -28,3 +28,68 @@ function Invoke-DscConfig {
     Assert-DscCheckSum
     Publish-DscConfig
 }
+
+function Download-DscResources {
+    param(
+        [string]$ResourceUrlCacheFile = (Join-Path -Path $PSDSC_TempPath -ChildPath 'PSDSC-ResourceDownloader.clixml')
+        ,
+        [switch]$IgnoreCachedUrls = $false
+        ,
+        [switch]$OverwriteExistingModules = $false
+    )
+
+    if (-Not (Test-Path -Path $ResourceUrlCacheFile) -Or $IgnoreCachedUrls) {
+        $ModuleList = New-Object System.Collections.ArrayList
+
+        $PageList = New-Object System.Collections.Stack
+        $PageList.Push('https://gallery.technet.microsoft.com/scriptcenter/site/search?f%5B0%5D.Type=Tag&f%5B0%5D.Value=Windows%20PowerShell%20Desired%20State%20Configuration&f%5B0%5D.Text=Windows%20PowerShell%20Desired%20State%20Configuration&pageIndex=1')
+        $PageBeenThere = New-Object System.Collections.ArrayList
+        while ($PageList.Count -gt 0) {
+            $url = $PageList.Pop()
+            if (-Not $PageBeenThere.Contains($url)) {
+                #'processing {0}' -f $url
+                $PageBeenThere.Add($url) | Out-Null
+                $page = Invoke-WebRequest $url
+
+                $page.Links | where {$_.href -match 'pageIndex' -and $_.innerText -match '\d+'} | foreach {
+                    $url = $_.href
+                    $url = $url.Replace('about:', 'https://gallery.technet.microsoft.com')
+                    $url = $url.Replace('&amp;', '&')
+                    if (-Not $PageBeenThere.Contains($url)) {
+                        $PageList.Push($url)
+                    }
+                }
+
+                $page.Links | where {$_.href -match '^about:/scriptcenter/(.+)-[a-z0-9]{8}$'} | foreach {
+                    $url = $_.href
+                    $url = $url.Replace('about:', 'https://gallery.technet.microsoft.com')
+                    $url = $url.Replace('&amp;', '&')
+                    $ModuleList.Push($url)
+                }
+
+                Start-Sleep -Seconds 5
+            }
+        }
+
+        $ModuleList | Export-Clixml -Path $ResourceUrlCacheFile
+
+    } else {
+        $ModuleList = Import-Clixml -Path $ResourceUrlCacheFile
+    }
+
+    Foreach ($ModuleUrl in $ModuleList) {
+        $page = Invoke-WebRequest $ModuleUrl
+        $page.Links | where {$_.href -match '^about:/scriptcenter/(.+-[a-z0-9]{8})/file/'} | select -First 1 | foreach {
+            $ItemName = $Matches[1]
+            $url = $_.href
+            $url = $url.Replace('about:', 'https://gallery.technet.microsoft.com')
+            $url = $url.Replace('&amp;', '&')
+            $url -match '/([^/]+.zip$)' | Out-Null
+            $FileName = $Matches[1]
+            $FileName = (Join-Path -Path $PSDSC_BasePath -ChildPath ('\DSC-Modules\' + $FileName))
+            if (-Not (Test-Path -Path $FileName) -Or $OverwriteExistingModules) {
+                Invoke-WebRequest $url -OutFile $FileName
+            }
+        }
+    }
+}
