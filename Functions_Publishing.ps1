bdc472e4981c21ad84b65c748bba3cf0872439fe
Nicholas Dille <nicholas.dille@mailbox.org>
2015-01-12 10:26:21 +0100

    Moved certificate enrollment to the library

--- Functions_Publishing.ps1
+++ Functions_Publishing.ps1
@@ -38,6 +38,48 @@ function Push-DscConfig {
     }
 }
 
+function New-Certificate {
+    $NewCertHostName = 'hv-05'
+    $NewCertCn       = ('{0}.demo.dille.name' -f $NewCertHostName)
+
+    $CertReqInfFile = Join-Path -Path $PSScriptRoot -ChildPath ('Cert\{0}_Req.inf' -f $NewCertHostName)
+    $CertReqFile    = Join-Path -Path $PSScriptRoot -ChildPath ('Temp\{0}_Req.req' -f $NewCertHostName)
+    $CertFile       = Join-Path -Path $PSScriptRoot -ChildPath ('Cert\{0}.cer' -f $NewCertHostName)
+    $CertRspFile    = Join-Path -Path $PSScriptRoot -ChildPath ('Temp\{0}_File.rsp' -f $NewCertHostName)
+    $CertPfxFile    = Join-Path -Path $PSScriptRoot -ChildPath ('Cert\{0}.pfx' -f $NewCertHostName)
+    $CertThumbFile  = Join-Path -Path $PSScriptRoot -ChildPath ('Cert\{0}.txt' -f $NewCertHostName)
+    $CredCertFile   = Join-Path -Path $PSScriptRoot -ChildPath 'Cred\Certificate.clixml'
+
+    if (Test-Path -Path $CertReqFile) {
+        Remove-Item -Path $CertReqFile
+    }
+    if (Test-Path -Path $CertFile) {
+        Remove-Item -Path $CertFile
+    }
+    if (Test-Path -Path $CertFile) {
+        Remove-Item -Path $CertFile
+    }
+
+    # create request
+    certreq.exe -new $CertReqInfFile $CertReqFile
+
+    # submit request
+    certreq.exe -config DC-01\demo-CA -submit $CertReqFile $CertFile
+
+    # import certificate
+    certreq.exe -accept $CertFile
+
+    # retrieve certificate thumbprint
+    $NewCert = Get-ChildItem Cert:\LocalMachine\My | where { $_.Subject -icontains $NewCertCn }
+    $NewCertThumb = $NewCert.Thumbprint
+
+    # Export certificate to pfx
+    Export-PfxCertificate -Cert $NewCert -FilePath $CertPfxFile -Password (Import-Clixml -Path $CredCertFile).Password
+
+    # Extract thumbprint
+    (Get-PfxData -FilePath C:\Users\administrator.DEMO\OneDrive\Scripts\DSC\Cert\contoso-dc-01.pfx -Password (Import-Clixml -Path $CredCertFile).Password).EndEntityCertificates.Thumbprint | Set-Content -Path CertThumbFile
+}
+
 function Set-VmConfiguration {
     param(
         [Parameter(Mandatory=$true)]
