ec324d68921d9c65dc6b41fe9ed5d65b5d30c750
Nicholas Dille <nicholas.dille@mailbox.org>
2015-01-14 21:40:39 +0100

    Generalized Set-VmCommand (issue #8)

--- Functions_Publishing.ps1
+++ Functions_Publishing.ps1
@@ -115,39 +115,197 @@ function Set-VmConfiguration {
         [Parameter(Mandatory=$false)]
         [string]
         $LocalBasePath = 'c:\dsc'
-        ,
-        [Parameter(Mandatory=$false)]
-        [string]
-        $IPv4Pattern = '^\d+\.\d+\.\d+\.\d+$'
     )
-    
-    $DomainCredFile = Join-Path -Path $PSDSC_CredPath   -ChildPath ($DomainCredName + '.clixml')
+
+    $CertCredName   = 'Certificates'
+    $CaFile         = Join-Path -Path $PSDSC_CertPath   -ChildPath ($RootCaName + '.cer')
     $CertFile       = Join-Path -Path $PSDSC_CertPath   -ChildPath ($VmName + '.pfx')
     $MetaFile       = Join-Path -Path $PSDSC_OutputPath -ChildPath ($NodeGuid + '.meta.mof')
 
-    $LocalCredFile  = Join-Path -Path $PSDSC_CredPath   -ChildPath ($LocalCredName + '.clixml')
-    $CertCredFile   = Join-Path -Path $PSDSC_CredPath   -ChildPath 'Certificates.clixml'
-    $CaFile         = Join-Path -Path $PSDSC_CertPath   -ChildPath ($RootCaName + '.cer')
-
     Enable-VMIntegrationService -ComputerName $VmHost -VMName $VmName -Name 'Guest Service Interface'
 
-    $Files = $($CertFile, $MetaFile, $CaFile)
-    $Files = foreach ($File in $Files) {
-        $File -imatch '^(\w)\:\\' | Out-Null
-        $File.Replace($Matches[0], '\\' + $env:COMPUTERNAME + '.' + $env:USERDNSDOMAIN + '\' + $Matches[1] + '$\')
+    #$PsSession = New-PsRemoteSession -ComputerName $VmIp -CredentialName $LocalCredName
+    $PsSession = New-PsRemoteSession -ComputerName $VmHost -CredentialName $DomainCredName
+    Import-Certificates -PsSession $PsSession -VmName $VmName -LocalCredentialName $LocalCredName `
+        -RootCertificate (Convert-RemoteFilePath -FilePath $CaFile) `
+        -ServerCertificate (Convert-RemoteFilePath -FilePath $CertFile) -ServerCertificateCredentialName $CertCredName
+    Set-DscMetaConfig -PsSession $PsSession -VmName $VmName -LocalCredentialName $LocalCredName -MetaConfig (Convert-RemoteFilePath -FilePath $MetaFile)
+}
+
+function Invoke-CommandOnFile {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true,ParameterSetName='Computer')]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $ComputerName
+        ,
+        [Parameter(Mandatory=$false,ParameterSetName='Computer')]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $CredentialName
+        ,
+        [Parameter(Mandatory=$true,ParameterSetName='PsSession')]
+        [ValidateNotNullOrEmpty()]
+        #[PsSession]
+        $PsSession
+        ,
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $VmName
+        ,
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $LocalCredentialName
+        ,
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string[]
+        $Files
+        ,
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        #[string]
+        $ScriptBlock
+        ,
+        [Parameter(Mandatory=$false)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $LocalBasePath = 'c:\dsc'
+    )
+
+    if (-Not $PsSession) {
+        $params = $PSBoundParameters
+        $params.Remove('MetaConfig')
+        $PsSession = New-PsRemoteSession @params
     }
-    Copy-VMFileRemotely -ComputerName $VmHost -Credential (Import-Clixml -Path $DomainCredFile) -VmName $VmName -Files $Files -DestinationPath $LocalBasePath
+    
+    Copy-VMFileRemotely -PsSession $PsSession -VmName $VmName -Files $Files -DestinationPath $LocalBasePath
+    $VmIp = Get-VmIp -ComputerName $VmHost -VmName $VmName
+    Invoke-Command -Session (New-PsRemoteSession -ComputerName $VmIp -CredentialName $LocalCredentialName) -ScriptBlock $ScriptBlock
+}
 
-    $Vm = Get-VM -ComputerName $VmHost -Name $VmName
-    $VmIp = $Vm.NetworkAdapters[0].IPAddresses | Where-Object { $_ -match $IPv4Pattern } | Select-Object -First 1
-    $CertPass = (Import-Clixml -Path $CertCredFile)
-    Invoke-Command -ComputerName $VmIp -Credential (Import-Clixml -Path $LocalCredFile) -ScriptBlock {
-        Get-ChildItem $Using:LocalBasePath\*.cer | foreach { Import-Certificate -FilePath $_.FullName -CertStoreLocation Cert:\LocalMachine\Root | Out-Null }
-        Get-ChildItem $Using:LocalBasePath\*.pfx | foreach { Import-PfxCertificate -FilePath $_.FullName -CertStoreLocation Cert:\LocalMachine\My -Password $Using:CertPass | Out-Null }
+function Set-DscMetaConfig {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true,ParameterSetName='Computer')]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $ComputerName
+        ,
+        [Parameter(Mandatory=$false,ParameterSetName='Computer')]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $CredentialName
+        ,
+        [Parameter(Mandatory=$true,ParameterSetName='PsSession')]
+        [ValidateNotNullOrEmpty()]
+        #[PsSession]
+        $PsSession
+        ,
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $VmName
+        ,
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $LocalCredentialName
+        ,
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $MetaConfig
+        ,
+        [Parameter(Mandatory=$false)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $LocalBasePath = 'c:\dsc'
+    )
+
+    if (-Not $PsSession) {
+        $params = $PSBoundParameters
+        $params.Remove('MetaConfig')
+        $PsSession = New-PsRemoteSession @params
+    }
+    
+    <#Copy-VMFileRemotely -PsSession $PsSession -VmName $VmName -Files ($MetaConfig) -DestinationPath $LocalBasePath
+    $VmIp = Get-VmIp -ComputerName $VmHost -VmName $VmName
+    Invoke-Command -Session (New-PsRemoteSession -ComputerName $VmIp -CredentialName $LocalCredentialName) -ScriptBlock {
         Get-ChildItem $Using:LocalBasePath\*.meta.mof | Where-Object { $_.BaseName -notmatch 'localhost.meta.mof' } | Select-Object -First 1 | Rename-Item -NewName localhost.meta.mof -ErrorAction SilentlyContinue
+        Set-DscLocalConfigurationManager -Path $Using:LocalBasePath -ComputerName localhost
+    }#>
 
+    Invoke-CommandOnFile -PsSession $PsSession -VmName $VmName -LocalCredentialName $LocalCredentialName -Files ($MetaConfig) -LocalBasePath $LocalCredentialName -ScriptBlock {
+        Get-ChildItem $Using:LocalBasePath\*.meta.mof | Where-Object { $_.BaseName -notmatch 'localhost.meta.mof' } | Select-Object -First 1 | Rename-Item -NewName localhost.meta.mof -ErrorAction SilentlyContinue
         Set-DscLocalConfigurationManager -Path $Using:LocalBasePath -ComputerName localhost
     }
+}
+
+function Import-Certificates {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true,ParameterSetName='Computer')]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $ComputerName
+        ,
+        [Parameter(Mandatory=$false,ParameterSetName='Computer')]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $CredentialName
+        ,
+        [Parameter(Mandatory=$true,ParameterSetName='PsSession')]
+        [ValidateNotNullOrEmpty()]
+        #[PsSession]
+        $PsSession
+        ,
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $VmName
+        ,
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $LocalCredentialName
+        ,
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $RootCertificate
+        ,
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $ServerCertificate
+        ,
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $ServerCertificateCredentialName
+        ,
+        [Parameter(Mandatory=$false)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $LocalBasePath = 'c:\dsc'
+    )
+
+    if (-Not $PsSession) {
+        $params = $PSBoundParameters
+        $params.Remove('MetaConfig')
+        $PsSession = New-PsRemoteSession @params
+    }
+    
+    $CertPass = Get-CredentialFromStore -CredentialName $ServerCertificateCredentialName
+    Invoke-Command -Session $PsSession -ScriptBlock {
+        Get-ChildItem $Using:LocalBasePath\*.cer | foreach { Import-Certificate    -FilePath $_.FullName -CertStoreLocation Cert:\LocalMachine\Root | Out-Null }
+        Get-ChildItem $Using:LocalBasePath\*.pfx | foreach { Import-PfxCertificate -FilePath $_.FullName -CertStoreLocation Cert:\LocalMachine\My -Password $Using:CertPass | Out-Null }
+    }
+
 }
 
 function Convert-DscMetaConfigurations {
