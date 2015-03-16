731e6d992d13e73697d42f837374e792ccce4cb7
Nicholas Dille <nicholas.dille@mailbox.org>
2015-03-16 11:44:25 +0100

    Separated the env config from the node config

---
+++ EnvironmentConfiguration.ps1
@@ -0,0 +1,4 @@
+﻿@{
+    WindowsSource            = '\\demo.dille.name\storage\install\Microsoft\Windows Server 2012 R2\WS12R2U1NovEN'
+    WindowsSourceCredentials = 'administrator@demo.dille.name'
+}
