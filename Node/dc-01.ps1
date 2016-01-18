46b64f5d617ea91b67f23458d19bae0c6fa41b90
Nicholas Dille <nicholas.dille@mailbox.org>
2016-01-18 23:19:51 +0100

    Updated nodes

--- Node/dc-01.ps1
+++ Node/dc-01.ps1
@@ -1,24 +1,24 @@
 ﻿@{
-    NodeName              = '0f45d854-94b9-469d-97f6-ab269320f92a'
+    NodeName                    = '0f45d854-94b9-469d-97f6-ab269320f92a'
+    PsDscAllowPlaintextPassword = 'true'
     Roles = @{
         Computer              = @{
-            MachineName = 'contoso-dc-01'
+            MachineName = 'dc-01'
         }
         VirtualMachine        = @{
             SwitchName       = 'Datacenter'
             State            = 'Running'
-            VhdxTemplateName = 'HyperV_Gen2_WS12R2U1_20140915.vhdx'
+            VhdxTemplateName = 'HyperV_Gen2_WS12R2U1_20150929_Core.vhdx'
             StartupMemory    = 512MB
             MinimumMemory    = 512MB
             MaximumMemory    = 2048MB
             ProcessorCount   = 2
         }
-        PullClientDebug      = @{}
         FirstDomainController = @{
-            DomainName  = 'contoso.com'
+            DomainName  = 'inmylab.de'
             RetryCount       = 3
             RetryIntervalSec = 10
-            Credentials = 'administrator@contoso.com'
+            Credentials = 'administrator@inmylab.de'
         }
     }
 }
--- Node/sql-01.ps1
+++ Node/sql-01.ps1
@@ -1,27 +1,28 @@
 ﻿@{
     NodeName              = '1ca1728d-f336-4772-bfa1-90b4758fc7f9'
     Roles = @{
-        Computer              = @{
-            MachineName = 'contoso-sql-01'
-            DomainName  = 'contoso.com'
-            Credentials = 'administrator@contoso.com'
-            DnsServer   = '10.0.0.112'
-            Adapter     = 'Ethernet 3'
-        }
         VirtualMachine        = @{
             SwitchName       = 'Datacenter'
             State            = 'Running'
-            VhdxTemplateName = 'HyperV_Gen2_WS12R2U1_20140915.vhdx'
+            VhdxTemplateName = 'HyperV_Gen2_WS12R2U1_20150929_Core.vhdx'
             StartupMemory    = 512MB
             MinimumMemory    = 512MB
             MaximumMemory    = 2048MB
             ProcessorCount   = 2
         }
-        PullClient            = @{}
+        Computer              = @{
+            MachineName = 'sql-01'
+            DomainName  = 'inmylab.de'
+            Credentials = 'administrator@inmylab.de'
+            DnsServer   = '10.0.0.112'
+            Adapter     = 'Ethernet 3'
+        }
+        # domain local group for local administrators 
+        # domain local group for instance administrators
         SqlStandalone = @{
             InstanceName     = 'MSSQLSERVER'
             #ServicePassword  = '43}G4t6Kp7hg:Wj'
-            Credential_Agent = 'Service-Sql-MSSQLSERVER@contoso.com'
+            Credential_Agent = 'Service-Sql-MSSQLSERVER@inmylab.de'
             Credential_SA    = 'sa@contoso-sql-01'
             Admins           = ('CONTOSO\Sql-Admins')
             SourcePath       = '\\demo.dille.name\storage\install\Microsoft\SQL Server 2012\SQL2012'
